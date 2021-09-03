a29d2804cd802a2d6b19d0036784d79fe39192c1
mohan1497 <r121497@rguktrkv.ac.in>
2021-09-03 14:36:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 this is for tesing only
 aganin test
 this is checking pull request
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World! this the game'); //write a response to the client
+  res.end(); //end the response
+}).listen(5000); //the server object listens on port 8080
