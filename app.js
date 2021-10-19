71e605cf8a29a2093d87ee32cc057bbb8a269bd1
mohan1497 <43566676+mohan1497@users.noreply.github.com>
2021-10-19 20:22:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ this is for tesing only
 aganin test
 this is checking pull request
 other org
+new code added aggia 
+#include.stdio.h
+main ()
+{
+  int a = 5;
+  int b = 10;
+  int c = a+b;
+  printf("%d",c)
+}
